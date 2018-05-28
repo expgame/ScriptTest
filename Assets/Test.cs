44cd4a2099fcc960b4d0742d4cc54f3711803467
expgame <exprobot@gmail.com>
2018-05-28 23:35:55 +0900

    課題の再提出。表記を簡略化しました。ご確認お願いします。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -5,14 +5,7 @@ public class Test : MonoBehaviour{
 	// Use this for initialization
 	void Start () {
 		//要素5の配列を初期化する
-		int [] array = new int[5];
-
-		//配列の各要素に値を代入する
-		array [0] = 100;
-		array [1] = 30;
-		array [2] = 20;
-		array [3] = 80;
-		array [4] = 10;
+		int [] array = {100, 30, 20, 80, 10};
 
 		//配列の要素をすべて表示する
 		for (int i = 4; i >= 0; i--) {
